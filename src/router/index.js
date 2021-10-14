e30cc4e7c835ef315b4571c5a700e20faf06088c
Alen <valekalen@gmail.com>
2021-10-14 22:41:22 +0200

    Adding Contact Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Repos from '../views/Repos.vue'
-import Projects from '../views/Projects.vue'
-import NotFound from '../views/NotFound.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Repos from '../views/Repos.vue';
+import Projects from '../views/Projects.vue';
+import NotFound from '../views/NotFound.vue';
+import ContactMe from '../views/ContactMe.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/repos',
-    name: 'Repos',
-    component: Repos
-  },
-  {
-    path: '/projects',
-    name: 'Projects',
-    component: Projects
-  },
-  {
-    path: '/:catchAll(.*)',
-    component: NotFound
-  }
-]
+   {
+      path: '/',
+      name: 'Home',
+      component: Home,
+   },
+   {
+      path: '/repos',
+      name: 'Repos',
+      component: Repos,
+   },
+   {
+      path: '/projects',
+      name: 'Projects',
+      component: Projects,
+   },
+   {
+      path: '/contact',
+      name: 'ContactMe',
+      component: ContactMe,
+   },
+   {
+      path: '/:catchAll(.*)',
+      component: NotFound,
+   },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+   history: createWebHistory(process.env.BASE_URL),
+   routes,
+});
 
-
-export default router
+export default router;
---
+++ src/views/ContactMe.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="container">
+      <form>
+         <input type="text" name="full-name" />
+         <input type="email" placeholder="E-mail" />
+         <textarea name="" id="" cols="30" rows="10"></textarea>
+      </form>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'ContactMe',
+};
+</script>
+
+<style></style>
