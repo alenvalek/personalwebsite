50db75e30439c4ed6bb88e2e87cd39d3cd3dde69
Alen <valekalen@gmail.com>
2021-10-14 22:42:47 +0200

    Added tailwindcss to the project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import './assets/tailwind.css'
 
 library.add(fab,fas)
 
